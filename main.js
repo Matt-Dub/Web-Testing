a22029b4e7cb208e3207d97023338d138f8adb5d
Mathieu <Matt@soundfingers.com>
2025-04-30 14:41:05 +0200

    added css fancy framework

--- main.js
+++ main.js
@@ -2,30 +2,7 @@ var mainButton = document.querySelector(".main-button");
 
 mainButton.addEventListener("click", changeTitle);
 
+
 function changeTitle(){
     document.querySelector(".page-h1").innerHTML = "Enter the Dark Tower";
 }
-
-function setMidWorldTheme() {
-    const hour = new Date().getHours();
-    const day = new Date().getDay();
-    const month = new Date().getMonth();
-    const year = new Date().getFullYear();
-    const detailsWrapper = document.getElementById(".details__wrapper");
-    const dateWrapper = document.querySelector(".details__main-block").firstChild();
-  
-    if (hour >= 5 && hour < 12) {
-      detailsWrapper.className = "dawn";
-      dateWrapper.innerHTML = "Gilead," + day + " " + month + " " + year + " - 3 pages - 29 sections."
-    } else if (hour >= 12 && hour < 18) {
-      detailsWrapper.className = "midday";
-    } else if (hour >= 18 && hour < 21) {
-      detailsWrapper.className = "dusk";
-    } else {
-      detailsWrapper.className = "night"; // Demon Moon time!
-    }
-  }
-  
-  // Run the function when the page loads
-  window.onload = setMidWorldTheme;
-  
--- midWorldTime.js
+++ midWorldTime.js
@@ -1,18 +1,25 @@
-function changeTitle(){
-    document.querySelector(".page-h1").innerHTML = "Enter the Dark Tower";
-}
+
+
+const midWorldMonths = [
+    "Snowbirth", "Thawsend", "Rainsinger", "Bloomtide",
+    "Sowingdeep", "Highsun", "Burning Moon", "Dustfall",
+    "Twilighting", "Falling Veil", "Ashend", "Longnight"
+  ];
 
 function setMidWorldTheme() {
     const hour = new Date().getHours();
     const day = new Date().getDay();
     const month = new Date().getMonth();
-    const year = new Date().getFullYear();
+    const year = new Date().getFullYear()-1472;
     const detailsWrapper = document.getElementById(".details__wrapper");
     const dateWrapper = document.querySelector("#date");
   
+    dateWrapper.innerHTML = "Gilead, " + day + " " + midWorldMonths[month] + " " + year + "Y.E.";
+
+    /*
     if (hour >= 5 && hour < 12) {
       detailsWrapper.className = ".dawn";
-      dateWrapper.innerHTML = "Gilead," + day + " " + month + " " + year + " - 3 pages - 29 sections."
+      
     } else if (hour >= 12 && hour < 18) {
       detailsWrapper.className = ".midday";
     } else if (hour >= 18 && hour < 21) {
@@ -20,6 +27,7 @@ function setMidWorldTheme() {
     } else {
       detailsWrapper.className = ".night"; // Demon Moon time!
     }
+      */
   }
   
   // Run the function when the page loads
