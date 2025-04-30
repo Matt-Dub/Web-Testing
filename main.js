82dcef0ffb977a75a4c3995afb7faed7f0015593
Mathieu <Matt@soundfingers.com>
2025-04-30 13:41:30 +0200

    added JS link to newspaper page

--- main.js
+++ main.js
@@ -8,10 +8,15 @@ function changeTitle(){
 
 function setMidWorldTheme() {
     const hour = new Date().getHours();
+    const day = new Date().getDay();
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
     const detailsWrapper = document.getElementById(".details__wrapper");
+    const dateWrapper = document.querySelector(".details__main-block").firstChild();
   
     if (hour >= 5 && hour < 12) {
       detailsWrapper.className = "dawn";
+      dateWrapper.innerHTML = "Gilead," + day + " " + month + " " + year + " - 3 pages - 29 sections."
     } else if (hour >= 12 && hour < 18) {
       detailsWrapper.className = "midday";
     } else if (hour >= 18 && hour < 21) {
